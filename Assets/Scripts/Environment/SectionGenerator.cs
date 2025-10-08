bceac689c71fd703573e99a933f69f01d9d858e9
MTGz17 <150878018+MTGz17@users.noreply.github.com>
2025-10-07 23:27:39 -0400

    Prototype-1

--- Assets/Scripts/Environment/SectionGenerator.cs
+++ Assets/Scripts/Environment/SectionGenerator.cs
@@ -11,7 +11,9 @@ public class SectionGenerator : MonoBehaviour
         {
             hasSpawned = true;
 
-            Vector3 spawnPosition = transform.position + transform.forward * 30f;
+            Vector3 offset = new Vector3(75f, 0f, 0f);
+
+            Vector3 spawnPosition = transform.position + transform.forward * 361.2826f + offset;
 
             Instantiate(roadSection, spawnPosition, Quaternion.identity);
         }
